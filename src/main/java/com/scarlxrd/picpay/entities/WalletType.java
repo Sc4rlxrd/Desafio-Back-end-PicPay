936885a3477ac6f0148f3d134eadd17c7660a68a
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2025-03-04 11:59:16 -0300

    feat: creation of entities

---
+++ src/main/java/com/scarlxrd/picpay/entities/WalletType.java
@@ -0,0 +1,58 @@
+package com.scarlxrd.picpay.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_wallet_type")
+public class WalletType {
+
+    @Id
+    private Long id;
+
+    private String description;
+
+    public WalletType() {
+    }
+
+    public WalletType(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public enum Enum {
+
+        USER(1L, "user"),
+        MERCHANT(2L, "merchant");
+
+        Enum(Long id, String description) {
+            this.id = id;
+            this.description = description;
+        }
+
+        private Long id;
+        private String description;
+
+        public WalletType get() {
+            return new WalletType(id, description);
+        }
+    }
+
+}
