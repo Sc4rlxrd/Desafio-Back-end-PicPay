801f8e1ff484ecd8d98714732bad7bd0d426799f
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2025-03-04 14:31:25 -0300

    feat: create walletDto

---
+++ src/main/java/com/scarlxrd/picpay/controller/dto/CreateWalletDto.java
@@ -0,0 +1,23 @@
+package com.scarlxrd.picpay.controller.dto;
+
+import com.scarlxrd.picpay.entities.Wallet;
+import com.scarlxrd.picpay.entities.WalletType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateWalletDto(@NotBlank String fullName,
+                              @NotBlank String cpfCnpj,
+                              @NotBlank String email,
+                              @NotBlank String password,
+                           @NotNull WalletType.Enum walletType
+) {
+    public Wallet toWallet() {
+        return new Wallet(
+                fullName,
+                cpfCnpj,
+                email,
+                password,
+                walletType.get()
+        );
+    }
+}
