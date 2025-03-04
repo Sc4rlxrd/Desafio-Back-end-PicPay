711f01e15b34a1cc127f63f28c90f60d0bf9f68d
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2025-03-04 12:01:58 -0300

    feat: creating command line to insert data into a database

---
+++ src/main/java/com/scarlxrd/picpay/config/DataLoader.java
@@ -0,0 +1,24 @@
+package com.scarlxrd.picpay.config;
+
+import com.scarlxrd.picpay.entities.WalletType;
+import com.scarlxrd.picpay.repository.WalletTypeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataLoader implements CommandLineRunner {
+
+    private final WalletTypeRepository walletTypeRepository;
+
+    public DataLoader(WalletTypeRepository walletRepository, WalletTypeRepository walletTypeRepository) {
+        this.walletTypeRepository = walletTypeRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.stream(WalletType.Enum.values())
+                .forEach(walletType -> walletTypeRepository.save(walletType.get()));
+    }
+}
